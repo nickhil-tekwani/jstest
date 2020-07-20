77c6eda18fa4b87eb74c052cd4cb31d3be274e6e
Nickhil Tekwani <44220251+nickhil-tekwani@users.noreply.github.com>
2020-07-20 01:55:48 -0700

    package init, pre publish

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# jstest
- 
+# @tekwani/jstest
+
+![npm (scoped)](https://img.shields.io/npm/v/@tekwani/jstest)
+![npm bundle size (scoped)](https://img.shields.io/bundlephobia/min/@tekwani/jstest)
+![GitHub](https://img.shields.io/github/license/nickhil-tekwani/jstest)
+
+Extremely lightweight testing library. My first npm package!
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const _ = require("underscore");
 
 module.exports = function isEqual(input, expectedResult) {
-  // if (!_isFunction(givenFunction)) throw new TypeError("first input is not a function!");
   return _isEqual(input, expectedResult);
 };
 
 module.exports = function isNotEqual(input, expectedResult) {
-  // if (!_isFunction(givenFunction)) throw new TypeError("first input is not a function!");
   return !_isEqual(input, expectedResult);
 };
